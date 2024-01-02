d94dce9f1486db5e6b0c792448fc2070f6eafcf3
YosraGD <yosraguedda@gmail.com>
2024-01-02 17:39:22 +0100

    Update course

--- src/App.js
+++ src/App.js
@@ -40,17 +40,25 @@ deleteCourse = (index) =>{
     courses})
   
 }
-
+//editCourse
+editCourse = (index, value) =>{
+let courses = this.state.courses;
+let course = courses[index];
+course['name'] = value
+this.setState({
+  courses
+})
+}
   render(){
 
     const {courses} = this.state;
     const courseList = courses.map((course,index)=>{
-      return <CourseList details = {course} key={index}  deleteCourse = {() => this.deleteCourse(index)}  />
+      return <CourseList details = {course} key={index} index ={index} deleteCourse = {() => this.deleteCourse(index)} editCourse={this.editCourse} />
     })
   return (
    <section className="App">
     <h2>Add courses</h2>
-   <CourseForm current={this.state.current}  updateCourse={this.updateCourse} addCourse={this.addCourse}  />
+   <CourseForm current={this.state.current}  updateCourse={this.updateCourse} addCourse={this.addCourse}   />
    <ul> {courseList}</ul>
    </section>
   );
--- src/Components/CourseList.js
+++ src/Components/CourseList.js
@@ -3,13 +3,47 @@ import React,{ Component, Fragment } from 'react';
 
 
 class CourseList extends Component {
+state={
+    isEdit : false
+}
+  // render course Item
+  renderCourse=()=>{
+    return(
+    <li>
+    <span>{this.props.details.name}</span>
+    <button onClick={() => {this.toggleState()}}>Edit course</button>
+    <button onClick={this.props.deleteCourse}>Delete course</button>
+    </li>)
+    
+  }
+  //updateCourseItem
+  updateCourseItem = (e) => {
+   e.preventDefault();
+   this.props.editCourse(this.props.index, this.input.value )
+   this.toggleState()
+  }
+  //render Update Form
+  renderUpdateForm=()=>{
+   return(
+    <form onSubmit={this.updateCourseItem}>
+        <input type='text' ref={(v) => this.input = v} defaultValue={this.props.details.name}/>
+        <button>Update course</button>
+    </form>
+   )
+  }
+//toggleState
+  toggleState=()=>{
+    let {isEdit} = this.state;
+    this.setState({
+        isEdit : !isEdit
+    })
+}
+
   render(){
+    let {isEdit} = this.state;
   return (
    <Fragment>
-    <li>{this.props.details.name}
-    <button onClick={this.props.deleteCourse}>Delete course</button>
-    </li>
-    
+    { isEdit ? this.renderUpdateForm() : this.renderCourse() }
     </Fragment>
   );
 }
